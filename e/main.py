5cbdcdb1ab4cc3d39ec6db28ed0b1ea166d92741
julian-velez <60607869+julian-velez@users.noreply.github.com>
2026-01-30 14:34:13 -0500

    ejemplo_3_python
    ejemplo_3_python

---
+++ e/main.py
@@ -0,0 +1,13 @@
+precio = 85  
+# Aquí se crea una variable llamada 'precio' y se le asigna el valor 85
+
+texto = f" el precio del producto es {precio:.3f} dolares"  
+# Esta es una f-string (cadena formateada)
+# {precio:.3f} significa:
+#  - 'precio' es la variable que se va a mostrar
+#  - '.3f' indica que se muestre como número decimal (float)
+#  - '3' es la cantidad de decimales que se quieren mostrar
+# Resultado: 85.000
+
+print(texto)  
+# Imprime en pantalla el contenido de la variable 'texto'
